2cadee2ea1a602145551352d6b006a655d5bc3ec
SohilNadarajan <32506686+SohilNadarajan@users.noreply.github.com>
2017-10-25 09:55:14 -0700

    Initial commit

---
+++ src/Board.java
@@ -0,0 +1,129 @@
+import processing.core.PApplet;
+import processing.core.PFont;
+
+public class Board extends PApplet {
+	private Pusher pusher1, pusher2;
+	private Puck puck;
+	boolean[] keys = new boolean[9];
+	
+	public Board() {
+		
+	}
+
+	public void draw() {
+		
+		act();
+		
+//		PFont font = createFont("Arial", 75);
+//		if (piece1.Xlife == 100) {
+//			textFont(font);
+//			text("PLAYER X WINS!", 300, 75);
+//		}
+//		if (piece1.Xlife == 0) {
+//			textFont(font);
+//			text("PLAYER SQUARE WINS!", 200, 75);
+//		}
+
+	}
+	
+	public void act() {
+		if (keys[0] == true) {
+			System.out.println("0");
+		}
+		if (keys[1] == true) {
+			System.out.println("1");
+		}
+		if (keys[2] == true) {
+			System.out.println("2");
+		}
+		if (keys[3] == true) {
+			System.out.println("3");
+		}
+		if (keys[4] == true) {
+			System.out.println("4");
+		}
+		if (keys[5] == true) {
+			System.out.println("5");
+		}
+		if (keys[6] == true) {
+			System.out.println("6");
+		}
+		if (keys[7] == true) {
+			System.out.println("7");
+		}
+	}
+	
+	public void keyPressed() {
+		if (key == 'w') {
+			keys[0] = true;
+		}
+		if (key == 'a') {
+			keys[1] = true;
+		}
+		if (key == 's') {
+			keys[2] = true;
+		}
+		if (key == 'd') {
+			keys[3] = true;
+		}
+		if (this.keyCode == 37) {
+			keys[4] = true;
+		}
+		if (this.keyCode == 38) {
+			keys[5] = true;
+		}
+		if (this.keyCode == 39) {
+			keys[6] = true;
+		}
+		if (this.keyCode == 40) {
+			keys[7] = true;
+		}
+		if (key == 'r') {
+			keys[8] = true;			
+		}
+		if (keys[8]) {
+//			RESTART KEY
+		}
+	}
+
+	public void keyReleased() {
+		if (key == 'w') {
+			keys[0] = false;
+		}
+		if (key == 'a') {
+			keys[1] = false;
+		}
+		if (key == 's') {
+			keys[2] = false;
+		}
+		if (key == 'd') {
+			keys[3] = false;
+		}
+		if (this.keyCode == 37) {
+			keys[4] = false;
+		}
+		if (this.keyCode == 38) {
+			keys[5] = false;
+		}
+		if (this.keyCode == 39) {
+			keys[6] = false;
+		}
+		if (this.keyCode == 40) {
+			keys[7] = false;
+		}
+		if (key == 'r') {
+			keys[8] = false;
+		}
+	}
+	
+	public void setup() {
+		
+	}
+}
+
+
+
+
+
+
+
---
+++ src/Hockey.java
@@ -0,0 +1,25 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+import processing.awt.PSurfaceAWT;
+import processing.core.PApplet;
+
+public class Hockey {
+	
+	
+	public static void main(String args[]) {
+		Board board = new Board();
+		PApplet.runSketch(new String[]{""}, board);
+		PSurfaceAWT surf = (PSurfaceAWT) board.getSurface();
+		PSurfaceAWT.SmoothCanvas canvas = (PSurfaceAWT.SmoothCanvas) surf.getNative();
+		JFrame window = (JFrame)canvas.getFrame();
+		
+		window.setLocation(0, 0);
+		window.setSize(1275, 700);
+		window.setMinimumSize(new Dimension(100,100));
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setResizable(true);
+		window.setVisible(true);
+	}
+}
